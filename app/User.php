c126e0f00ed0f2e82617441d8ea2921a654c3191
2kpo <395357821@qq.com>
2017-06-09 10:53:43 +0800

    登录注册重置密码功能

--- app/User.php
+++ app/User.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Naux\Mail\SendCloudTemplate;
+use Mail;
 
 class User extends Authenticatable
 {
@@ -15,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'avatar', 'confirmation_token'
     ];
 
     /**
@@ -26,4 +28,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $data = ['url' => url('password/reset', $token),];
+        $template = new SendCloudTemplate('zhihu_reset_password', $data);
+        Mail::raw($template, function ($message) {
+            $message->from('2kpo@zhihu-dev.com', '2kpo');
+            $message->to($this->email);
+        });
+    }
 }
